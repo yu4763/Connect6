671069ea23ad8bf1626dd26e2f4a2fc8dcc0f62f
L2zz <ulla4571@gmali.com>
2018-10-06 09:33:57 +0900

    Comment out for testing: Select Iterative and no threads

--- monte_carlo_tree_search.cpp
+++ monte_carlo_tree_search.cpp
@@ -1,7 +1,7 @@
 #include "monte_carlo_tree_search.h"
 using namespace std;
 
-// 
+//
 extern board window;
 
 //
@@ -34,54 +34,54 @@ Position MonteCarloTreeSearch(State& current) {
   return best_pos.GetPostion();
 }
 
-// Recursive version of Selection and Expansion 
-State& State::SelectionAndExpansion() {
-  // If there is no child state, then expand and return its child.
-  // If all children are visited, then go to best child.
-  int max_idx;
-  double max_uct_value = 0;
-  for (int i = 0; i < mct_const::NUMBER_OF_MAX_CHILD_NODES; i++) {
-    if (child_list_[i] == NULL) {
-      // For Arbitrary child construction
-      // ==========================================
-      int cidx_list[10];
-      int cidx_num = 0;
-      int idx = 0;
-      while (1) {
-        if (cidx_num == 10) break;
-        if (idx == 361) {
-          cout << "There is no space to play" << endl;
-          exit(1);
-        }
-        if (board_[idx] == 0) {
-          cidx_list[cidx_num++] = idx;
-        }
-        idx++;
-      } 
-      // =========================================
-      for (int j = 0; j < mct_const::NUMBER_OF_MAX_CHILD_NODES; j++) {
-        //
-        // Make child state with our strategy MakeChildState();
-        // Save change index
-        //
-        
-        // Make with black
-        MakeChildState(j, cidx_list[2 * j], cidx2[2 * j + 1], 1);
-      }
-      return *child_list_[i];
-    }
-    // For non visited node
-    if (child_list_[i]->number_of_visiting_ == 0) return *child_list_[i];
-
-    // Compare uct_values to choose best child
-    if (child_list_[i]->uct_value_ > max_uct_value) {
-      max_idx = i;
-      max_uct_value = child_list_[i]->uct_value_;
-    }
-  }
-
-  return child_list_[max_idx]->SelectionAndExpansion();
-}
+// Recursive version of Selection and Expansion
+// State& State::SelectionAndExpansion() {
+//   // If there is no child state, then expand and return its child.
+//   // If all children are visited, then go to best child.
+//   int max_idx;
+//   double max_uct_value = 0;
+//   for (int i = 0; i < mct_const::NUMBER_OF_MAX_CHILD_NODES; i++) {
+//     if (child_list_[i] == NULL) {
+//       // For Arbitrary child construction
+//       // ==========================================
+//       int cidx_list[10];
+//       int cidx_num = 0;
+//       int idx = 0;
+//       while (1) {
+//         if (cidx_num == 10) break;
+//         if (idx == 361) {
+//           cout << "There is no space to play" << endl;
+//           exit(1);
+//         }
+//         if (board_[idx] == 0) {
+//           cidx_list[cidx_num++] = idx;
+//         }
+//         idx++;
+//       }
+//       // =========================================
+//       for (int j = 0; j < mct_const::NUMBER_OF_MAX_CHILD_NODES; j++) {
+//         //
+//         // Make child state with our strategy MakeChildState();
+//         // Save change index
+//         //
+//
+//         // Make with black
+//         MakeChildState(j, cidx_list[2 * j], cidx2[2 * j + 1], 1);
+//       }
+//       return *child_list_[i];
+//     }
+//     // For non visited node
+//     if (child_list_[i]->number_of_visiting_ == 0) return *child_list_[i];
+//
+//     // Compare uct_values to choose best child
+//     if (child_list_[i]->uct_value_ > max_uct_value) {
+//       max_idx = i;
+//       max_uct_value = child_list_[i]->uct_value_;
+//     }
+//   }
+//
+//   return child_list_[max_idx]->SelectionAndExpansion();
+// }
 
 // Iterative version of Selection and Expansion
 State& State::SelectionAndExpansion() {
@@ -115,7 +115,7 @@ State& State::SelectionAndExpansion() {
           // Make child state with our strategy MakeChildState();
           // Save change index
           //
-        
+
           // Make with black
           best_child->MakeChildState(born, empty_list[2 * born], empty_list[2 * born + 1], 1);
         }
@@ -124,8 +124,8 @@ State& State::SelectionAndExpansion() {
       // Case2) There is unvisited node
       } else if (best_child->child_list_[child_idx]->number_of_visiting_ == 0) {
         return *(best_child->child_list_[child_idx]);
-      
-      // Case3) There is node who has larger uct_value_ than max 
+
+      // Case3) There is node who has larger uct_value_ than max
       } else if (best_child->child_list_[child_idx]->uct_valuee_ > max_uct_value) {
         max_idx = child_idx;
         max_uct_value = best_child->child_list_[child_idx]->uct_value_;
@@ -140,36 +140,36 @@ State& State::SelectionAndExpansion() {
 void State::MakeChildState(const int child_idx, const int idx_1, const int idx_2, const char color) {
   child_list_[child_idx] = new State(*this);
   child_list_[child_idx]->parent = this;
-  
+
   // Set with my_color
   child_list_[child_idx]->board_[idx_1] = color;
   child_list_[child_idx]->board_[idx_2] = color;
-  
+
   child_list_[chlid_idx]->change_idx_1 = idx_1;
   child_list_[child_idx]->change_idx_2 = idx_2;
 }
 
 // With Thread
-int State::Evaluation() {
-  int win_count = 0;
-  thread** thread_list = new thread*[mct_const::NUMBER_OF_THREADS];
-
-  for (int i = 0; i < mct_const::NUMBER_OF_THREADS; i++) {
-    thread_list[i] = new thread(VirtualPlay, win_count);
-  }
-
-  for (int i = 0; i < mct_const::NUMBER_OF_THREADS; i++) {
-    thread_list[i]->join();
-  }
-  delete[] thread_list;
-
-  return win_count;
-}
+// int State::Evaluation() {
+//   int win_count = 0;
+//   thread** thread_list = new thread*[mct_const::NUMBER_OF_THREADS];
+//
+//   for (int i = 0; i < mct_const::NUMBER_OF_THREADS; i++) {
+//     thread_list[i] = new thread(VirtualPlay, win_count);
+//   }
+//
+//   for (int i = 0; i < mct_const::NUMBER_OF_THREADS; i++) {
+//     thread_list[i]->join();
+//   }
+//   delete[] thread_list;
+//
+//   return win_count;
+// }
 
 // Without Thread
 int State::Evaluation() {
   int win_count = 0;
-  
+
   VirtualPlay(win_count);
 
   return win_count;
@@ -217,19 +217,19 @@ void State::VirtualPlay(int& win_count) {
 }
 
 // With thread
-void State::Update(int result) {
-  number_of_wins_ += result;
-  number_of_visiting_ += mct_const::NUMBER_OF_THREADS;
-  
-  // Base state do not need to update uct value
-  if (parent_ != NULL) {
-    uct_value_ += 2 * log(parent_->number_of_visiting_ + mct_const::NUMBER_OF_THREADS);
-    uct_value_ /= number_of_visiting_;
-    uct_value_ = sqrt(uct_value_);
-    uct_value_ += number_of_wins_ / number_of_visiting_;
-  } 
-  parent_->Update();
-}
+// void State::Update(int result) {
+//   number_of_wins_ += result;
+//   number_of_visiting_ += mct_const::NUMBER_OF_THREADS;
+//
+//   // Base state do not need to update uct value
+//   if (parent_ != NULL) {
+//     uct_value_ += 2 * log(parent_->number_of_visiting_ + mct_const::NUMBER_OF_THREADS);
+//     uct_value_ /= number_of_visiting_;
+//     uct_value_ = sqrt(uct_value_);
+//     uct_value_ += number_of_wins_ / number_of_visiting_;
+//   }
+//   parent_->Update();
+// }
 
 // Iterative Version of Update without threads
 void State::Update(int result) {
@@ -238,7 +238,7 @@ void State::Update(int result) {
     if (current_state == NULL) break;
     current_state->number_of_wins += result;
     current_state->numver_of_visiting++;
-    
+
     // Base state do not need to update uct value
     if (parent_ != NULL) {
       current_state->uct_value_ += 2 * log(parent_->number_of_visiting_ + 1);
@@ -269,11 +269,11 @@ Position State::BestChoice() {
 }
 
 int IsWin(const char* board) {
-  
+
   // ===========================
   // For arbitrary win decision
   //
-  if (rand() & 1) { 
+  if (rand() & 1) {
     return 0;
   } else {
     return 1;
